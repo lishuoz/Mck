d18fb53aff60679c8688a532b158c84fea95c663
lishuoz <zhanglishuo13@gmail.com>
2018-08-20 11:39:59 -0400

    change level name

--- database/factories/LevelFactory.php
+++ database/factories/LevelFactory.php
@@ -4,7 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Level::class, function (Faker $faker) {
 	return [
-		'name' => $faker->unique()->randomElement($array = array ('比赛使用 - 已图片匹配 （Photomatched GU）', '比赛使用（GU）', '比赛使用/备用（GU/GI）', '比赛/球队/工厂备用（GI/TI/FI）', '市售（PC）'))
+		'name' => $faker->unique()->randomElement($array = array ('比赛使用 - 已图片匹配 （Photomatched GU）', '比赛使用（GU）', '比赛使用/备用（GU/GI）', '比赛/球队/工厂使用（GI/TI/FI）', '市售（PC）'))
 
 	];
 });
